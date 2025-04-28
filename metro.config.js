872a75feee630b9072db1e08af1315e6c80351de
bambooka <s.olga@topnlab.ru>
2025-04-28 15:04:44 +0300

    rename app

--- metro.config.js
+++ metro.config.js
@@ -6,6 +6,16 @@ const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
  *
  * @type {import('metro-config').MetroConfig}
  */
-const config = {};
+const config = getDefaultConfig(__dirname);
+
+config.transformer = {
+    ...config.transformer,
+    babelTransformerPath: require.resolve('react-native-svg-transformer'),
+  };
+  config.resolver = {
+    ...config.resolver,
+    assetExts: config.resolver.assetExts.filter(ext => ext !== 'svg'),
+    sourceExts: [...config.resolver.sourceExts, 'svg'],
+  };
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
